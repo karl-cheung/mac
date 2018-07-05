cb9a2cd983d0cb0cc3fd67757e1151acbb407f5c
Zhang Yu <zhangyu.karl@gmail.com>
2018-07-05 17:25:46 +0800

    update

---
+++ public/apm.sh
@@ -0,0 +1,51 @@
+# !/bin/bash
+
+# Awesome Atom Packages
+# Before you run this shell script, make sure you have installed Atom
+
+apm install \
+atom-material-syntax \
+atom-material-syntax-light \
+atom-material-ui \
+atom-ternjs \
+atom-beautify \
+autocomplete-modules \
+autocomplete-paths \
+atom-html-preview \
+atom-css-comb \
+atom-ternjs \
+color-picker \
+docblockr \
+emmet \
+editorconfig \
+file-icons \
+git-plus \
+highlight-selected \
+hyperclick \
+linter \
+linter-eslint \
+language-babel \
+language-postcss \
+language-vue \
+less-autocompile \
+js-hyperclick \
+merge-conflicts \
+minimap \
+minimap-highlight-selected \
+markdown-preview-plus \
+minimap-git-diff \
+open-in-browser \
+project-manager \
+pigments \
+platformio-ide-terminal \
+sync-settings \
+sass-autocompile \
+simplified-chinese-menu \
+svn \
+seti-ui \
+tree-view-finder \
+travis-ci-status \
+vim-mode \
+
+
+
